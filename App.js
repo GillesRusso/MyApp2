9d034870ce9112770d88d23e6f8a26630c348526
Gilles Russo <russogilles5@gmail.com>
2022-07-07 13:53:17 +0200

    changed variable

---
+++ App.js
@@ -0,0 +1,67 @@
+import 'react-native-gesture-handler';
+import React, { useEffect, useState } from 'react';
+import { NativeBaseProvider } from 'native-base';
+import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Calendar, CalendarList, Agenda, LocaleConfig, calendarTheme } from 'react-native-calendars';
+import HomeScreen from './screens/HomeScreen';
+import Calendars from './screens/CalendarScreen';
+import MyPedometer from './components/MyPedometer';
+import MyToDoList from './components/MyToDoList';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+ 
+  const vacation = { key: 'vacation', color: 'red', selectedDotColor: 'blue' };
+  const massage = { key: 'massage', color: 'blue', selectedDotColor: 'blue' };
+  const workout = { key: 'workout', color: 'green' };
+
+  LocaleConfig.locales['fr'] = {
+    monthNames: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ],
+    monthNamesShort: ['Jan.', 'Feb.', 'Mar', 'Apr', 'May', 'June', 'Jul.', 'Aug', 'Sep.', 'Oct.', 'Nov.', 'Dec.'],
+    dayNames: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    dayNamesShort: ['Mon.', 'Tue.', 'Wed.', 'Thu.', 'Fri.', 'Sat.', 'Sun.'],
+    today: "Today"
+  };
+  LocaleConfig.defaultLocale = 'fr';
+
+  return (
+    <NavigationContainer>
+      <NativeBaseProvider>
+        <Stack.Navigator initialRouteName='Home'>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={Calendars} />
+          <Stack.Screen name="MyPedometer" component={MyPedometer} />
+          <Stack.Screen name="MyToDoList" component={MyToDoList} />
+        </Stack.Navigator>
+      </NativeBaseProvider>
+    </NavigationContainer>
+
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    flexDirection: "column",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ screens/CalendarScreen.js
@@ -0,0 +1,53 @@
+import { Calendar, CalendarList, Agenda, LocaleConfig, calendarTheme } from 'react-native-calendars';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+
+
+export default function Calendars({ navigation }) {
+    return (
+        <View style={styles.center}>
+            <CalendarList
+                onVisibleMonthsChange={(months) => { console.log('now these months are visible', months); }}
+                pastScrollRange={50}
+                futureScrollRange={50}
+                scrollEnabled={true}
+                showScrollIndicator={true}
+                markingType={'custom'}
+                markedDates={{
+                    '2018-03-28': {
+                        customStyles: {
+                            container: {
+                                backgroundColor: 'green'
+                            },
+                            text: {
+                                color: 'black',
+                                fontWeight: 'bold'
+                            }
+                        }
+                    },
+                    '2018-03-29': {
+                        customStyles: {
+                            container: {
+                                backgroundColor: 'white',
+                                elevation: 3
+                            },
+                            text: {
+                                color: 'blue'
+                            }
+                        }
+                    }
+                }}
+
+            />
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        flexDirection: "column",
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
